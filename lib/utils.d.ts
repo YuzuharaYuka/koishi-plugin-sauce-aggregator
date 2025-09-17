1582fd5b471cc78a96007d4831884707df6f27eb
YuzuharaYuka <2591169349@qq.com>
2025-09-17 22:26:33 +0800

    build: Ignore build artifacts and dependencies

--- lib/utils.d.ts
+++
@@ -1,9 +0,0 @@
-import { Buffer } from 'buffer';
-export declare const USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36";
-export declare function getImageTypeFromUrl(url: string): string;
-export declare function detectImageType(buffer: Buffer): 'jpeg' | 'png' | null;
-export declare function preprocessImage(buffer: Buffer, maxSizeInMB?: number): Promise<Buffer>;
-export declare function getImageUrlAndName(session: any, text: string): {
-    url: string;
-    name: string;
-};
